afa76576150c86941e8c8b521f06e9dccc4bd1b1
Jander Silva <jander.silv@outlook.com>
2022-03-07 13:08:02 -0300

    feat: initial implementation of the robot.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,237 @@
+import { useEffect, useRef } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import classes from 'styles/home.module.css'
+
+const TILE_AREA = 64
+const NUM_TILE = 8
+
+const CANVAS_AREA = TILE_AREA * NUM_TILE
+const DEFAULT_SPEED = 64
+
+enum robotMovement {
+  forward,
+  backward,
+  left,
+  right,
+}
+
+enum robotMemory {
+  go = 0,
+  avoid,
+  alreadyWalked,
+}
+
+enum objects {
+  empty = 0,
+  wall,
+  dust,
+  furniture,
+}
+
+let robotDirection = robotMovement.forward
+
+let xPosition = 0
+let yPosition = 0
+let xSpeed = 0
+let ySpeed = -DEFAULT_SPEED
+
+let vacuumImage: HTMLImageElement | null = null
+let vacuumArea = 60
+
+let dustImage: HTMLImageElement | null = null
+let dustArea = 25
+
+const world: number[][] = []
+const robotMemoryMap: number[][] = []
+
+let interval = {} as NodeJS.Timeout
+
+const getXPosition = (canvasWidth: number) => canvasWidth
+const getYPosition = (canvasHeight: number) => canvasHeight
+
+const resetGame = (canvas: HTMLCanvasElement) => {
+  xPosition = getXPosition(canvas.width)
+  yPosition = getYPosition(canvas.height)
+  xSpeed = 0
+  ySpeed = -DEFAULT_SPEED
+}
+
+const setupCanvas = (canvas: HTMLCanvasElement) => {
+  // canvas.width = window.innerWidth
+  // canvas.height = window.innerHeight
+  canvas.width = CANVAS_AREA
+  canvas.height = CANVAS_AREA
+
+  xPosition = getXPosition(canvas.width)
+  yPosition = getYPosition(canvas.height)
+
+  return canvas.getContext('2d')
+}
+
+const loadRobotMemory = () => {
+  for (let i = 0; i < NUM_TILE; i++) {
+    robotMemoryMap[i] = []
+    for (let j = 0; j < NUM_TILE; j++) {
+      robotMemoryMap[i][j] = robotMemory.go
+    }
+  }
+  console.log({ robotMemoryMap })
+}
+
+const loadWorld = () => {
+  for (let i = 0; i < NUM_TILE; i++) {
+    world[i] = []
+    for (let j = 0; j < NUM_TILE; j++) {
+      world[i][j] = objects.empty
+      if (Math.random() > 0.9) world[i][j] = objects.dust
+    }
+  }
+  world[7][5] = objects.furniture
+  console.log({ world })
+}
+
+const loadVacuumImage = () => {
+  const vacuumImageElement = new Image()
+  vacuumImageElement.src = '/assets/vacuum.png'
+  vacuumImageElement.onload = () => {
+    vacuumImage = vacuumImageElement
+    vacuumArea = vacuumImageElement.width
+  }
+}
+
+const loadDustImage = () => {
+  const dustImageElement = new Image()
+  dustImageElement.src = '/assets/dust.png'
+  dustImageElement.onload = () => {
+    dustImage = dustImageElement
+    dustArea = dustImageElement.width
+  }
+}
+
+const drawWorld = (ctx: CanvasRenderingContext2D) => {
+  for (let i = 0; i < NUM_TILE; i++) {
+    for (let j = 0; j < NUM_TILE; j++) {
+      switch (world[i][j]) {
+        case objects.dust:
+          if (!dustImage) return
+          ctx.drawImage(dustImage, i * TILE_AREA, j * TILE_AREA, TILE_AREA / 2, TILE_AREA / 2)
+          break
+        default:
+          break
+      }
+    }
+  }
+}
+
+const drawFurniture = (ctx: CanvasRenderingContext2D) => {
+  ctx.beginPath()
+  ctx.rect(7 * TILE_AREA, 5 * TILE_AREA, TILE_AREA, TILE_AREA)
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+
+const drawRobot = (ctx: CanvasRenderingContext2D) => {
+  if (!vacuumImage) return
+  ctx.drawImage(vacuumImage, xPosition - vacuumArea, yPosition - vacuumArea)
+}
+
+const checkIsFree = (positionValue: number) => [objects.empty, objects.dust].includes(positionValue)
+
+const draw = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D | null) => {
+  if (!ctx) return
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  drawWorld(ctx)
+  drawRobot(ctx)
+  drawFurniture(ctx)
+
+  const tileXPosition = Math.floor(xPosition / TILE_AREA) - 1
+  const tileYPosition = Math.floor(yPosition / TILE_AREA) - 1
+
+  // console.log({
+  //   xPosition,
+  //   yPosition,
+  //   tileXPosition,
+  //   tileYPosition,
+  //   xSpeed,
+  //   ySpeed,
+  //   canvasWidth: canvas.width,
+  //   canvasHeight: canvas.height,
+  // })
+
+  if (robotDirection === robotMovement.forward) {
+    if (checkIsFree(world[tileXPosition][tileYPosition - 1])) {
+      robotDirection = robotMovement.forward
+      xSpeed = 0
+      ySpeed = -DEFAULT_SPEED
+    } else if (checkIsFree(world[tileXPosition - 1][tileYPosition])) {
+      robotDirection = robotMovement.left
+      xSpeed = -DEFAULT_SPEED
+      ySpeed = 0
+    } else if (checkIsFree(world[tileXPosition + 1][tileYPosition])) {
+      robotDirection = robotMovement.right
+      xSpeed = DEFAULT_SPEED
+      ySpeed = 0
+    } else {
+      robotDirection = robotMovement.backward
+      xSpeed = 0
+      ySpeed = -DEFAULT_SPEED
+    }
+  }
+
+  if (xPosition + xSpeed < vacuumArea) {
+    ySpeed = -DEFAULT_SPEED
+    xSpeed = 0
+  }
+  if (xPosition + xSpeed > canvas.width) {
+    ySpeed = DEFAULT_SPEED
+    xSpeed = 0
+  }
+  if (yPosition + ySpeed < vacuumArea) {
+    ySpeed = 0
+    xSpeed = DEFAULT_SPEED
+  }
+  if (yPosition + ySpeed > canvas.height) {
+    ySpeed = 0
+    xSpeed = -DEFAULT_SPEED
+  }
+
+  xPosition += xSpeed
+  yPosition += ySpeed
+}
 
 const Home: NextPage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const ctx = setupCanvas(canvas)
+
+    loadVacuumImage()
+    loadDustImage()
+
+    loadWorld()
+    loadRobotMemory()
+    // loadDustAtWorld()
+
+    interval = setInterval(() => draw(canvas, ctx), 1000)
+    return () => clearInterval(interval)
+  }, [canvasRef])
+
   return (
-    <div className={styles.container}>
+    <main className={classes.main}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Vacuum Cleaner</title>
+        <meta name="description" content="Vacuum Cleaner" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      <canvas ref={canvasRef} width="300" height="300" className={classes.canvas}>
+        <p>It was not possible load the canvas</p>
+      </canvas>
+    </main>
   )
 }
 
