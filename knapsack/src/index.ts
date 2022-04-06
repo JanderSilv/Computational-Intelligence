625363d606c731254ec1b968ccb7b49c04b570aa
Jander Silva <jander.silv@outlook.com>
2022-04-06 00:45:18 -0300

    ✨feat: implements partially the selection, crossover and mutation functions.

--- knapsack/src/index.ts
+++ knapsack/src/index.ts
@@ -1,4 +1,4 @@
-import { Item } from './types'
+import { Chromosome, IndexedChromosome, Item, Population } from './types'
 
 const generations = 5
 const knapsackMaxWeight = 15
@@ -20,52 +20,95 @@ const itens: Item[] = [
 ]
 
 const makeChromosome = (itens: Item[]) => {
-  const chromosome: number[] = []
+  const chromosome: Chromosome = []
   for (let i = 0; i < itens.length; i++) chromosome.push(Math.round(Math.random()))
   return chromosome
 }
 
 const makePopulation = () => {
-  const population: number[][] = []
+  const population: Population = []
   for (let i = 0; i < generations; i++) population.push(makeChromosome(itens))
   return population
 }
 
-const fitness = (chromosome: number[]) => {
+const fitness = (chromosome: Chromosome) => {
   let totalValue = 0
   const totalWeight = chromosome.reduce((acc, gene, index) => {
     if (!gene) return acc
     totalValue += itens[index].value
     return acc + itens[index].weight
   }, 0)
-  return totalWeight <= knapsackMaxWeight ? totalValue : 0
+  if (totalWeight > knapsackMaxWeight || totalValue === 0) return 1
+  return totalValue
 }
 
-// create a wheel roulette
-// the values of the roulette are the fitness of each chromosome
-// the higher the fitness, the bigger the chance to be selected
-// return the indexes of the chromosomes that will be selected
-const rouletteWheel = (population: number[][], fitnesses: number[]) => {
+const rouletteWheel = (population: Population, fitnesses: number[]) => {
   const totalFitness = fitnesses.reduce((acc, fitness) => acc + fitness, 0)
-  const wheel: number[] = []
-  for (let i = 0; i < population.length; i++) {
-    const fitness = fitnesses[i]
-    for (let j = 0; j < fitness; j++) wheel.push(i)
-  }
-  const wheelSize = wheel.length
-  const wheelIndexes: number[] = []
-  for (let i = 0; i < population.length; i++) {
-    const index = Math.floor(Math.random() * wheelSize)
-    wheelIndexes.push(wheel[index])
-  }
-  return wheelIndexes
+  const probabilities = fitnesses.map(fitness => fitness / totalFitness)
+
+  const roulette: number[] = []
+  probabilities.reduce((acc, probability) => {
+    const sum = acc + probability
+    roulette.push(sum)
+    return sum
+  }, 0)
+
+  const random = Math.random()
+  const foundIndex = roulette.findIndex(value => random <= value)
+  // console.log({ roulette, random, foundIndex })
+  return population[foundIndex]
 }
 
-const main = () => {
+const makeCrossover = (chromosome1: IndexedChromosome, chromosome2: IndexedChromosome) => {
+  const crossoverPoint = Math.floor(Math.random() * chromosome1.chromosome.length)
+  const offSpring1: IndexedChromosome = { index: chromosome1.index, chromosome: [] }
+  const offSpring2: IndexedChromosome = { index: chromosome2.index, chromosome: [] }
+  for (let i = 0; i < chromosome1.chromosome.length; i++) {
+    if (i < crossoverPoint) {
+      offSpring1.chromosome.push(chromosome1.chromosome[i])
+      offSpring2.chromosome.push(chromosome2.chromosome[i])
+    } else {
+      offSpring1.chromosome.push(chromosome2.chromosome[i])
+      offSpring2.chromosome.push(chromosome1.chromosome[i])
+    }
+  }
+  return { offSpring1, offSpring2 }
+}
+
+const makeMutation = (chromosome: Chromosome) => {
+  const mutationPoint = Math.floor(Math.random() * chromosome.length)
+  const mutatedChromosome: number[] = []
+  for (let i = 0; i < chromosome.length; i++) {
+    if (i === mutationPoint) mutatedChromosome.push(chromosome[i] === 0 ? 1 : 0)
+    else mutatedChromosome.push(chromosome[i])
+  }
+  return mutatedChromosome
+}
+
+const getRandomChromosome = (population: Population): IndexedChromosome => {
+  const index = Math.floor(Math.random() * population.length)
+  return { index, chromosome: population[index] }
+}
+
+const handleGeneration = () => {
   const population = /* makePopulation() */ mockedPopulation
   const fitnesses = population.map(chromosome => fitness(chromosome))
-  const roulette = rouletteWheel(population, fitnesses)
-  console.log(fitnesses)
+  const selectedChromosomes: number[][] = []
+  for (let i = 0; i < fitnesses.length; i++) {
+    const selectedChromosome = rouletteWheel(population, fitnesses)
+    selectedChromosomes.push(selectedChromosome)
+  }
+
+  const { offSpring1, offSpring2 } = makeCrossover(
+    getRandomChromosome(selectedChromosomes),
+    getRandomChromosome(selectedChromosomes)
+  )
+
+  // population.splice(offSpring1.index, 1, offSpring1.chromosome)
+  // population.splice(offSpring2.index, 1, offSpring2.chromosome)
+
+  // const mutatedOffSpring1 = Math.random() > 0.7 ? makeMutation(offSpring1) : offSpring1
+  console.log({ population, fitnesses, selectedChromosomes })
 }
 
-main()
+handleGeneration()
--- knapsack/src/types.ts
+++ knapsack/src/types.ts
@@ -2,3 +2,12 @@ export type Item = {
   value: number
   weight: number
 }
+
+export type Chromosome = number[]
+
+export type IndexedChromosome = {
+  index: number
+  chromosome: Chromosome
+}
+
+export type Population = Chromosome[]
