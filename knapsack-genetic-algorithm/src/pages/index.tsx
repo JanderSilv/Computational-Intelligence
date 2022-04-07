6b89bb2ba3efb30481661b15490c8435c60f1691
Jander Silva <jander.silv@outlook.com>
2022-04-07 04:57:00 -0300

    ✨ feat: finishes the essencial of the interface.

--- knapsack-genetic-algorithm/src/pages/index.tsx
+++ knapsack-genetic-algorithm/src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
 import type { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+
 import { solveKnapsack } from 'src/helpers'
 import { Generation as TGeneration, Item as TItem, KnapsackSolution } from 'src/helpers/types'
+import classes from 'src/styles/home.module.css'
+
+const removeNonNumeric = (num: string) => num.toString().replace(/[^0-9]/g, '')
+const formatNumber = (num: string) => removeNonNumeric(num)
 
 const Item = (props: TItem & { label: string }) => {
   const { value, weight, label } = props
@@ -8,17 +15,18 @@ const Item = (props: TItem & { label: string }) => {
 }
 
 const Generation = (props: TGeneration) => {
-  const { index, population } = props
+  const { index, population, totalFitness } = props
   return (
     <div>
       <h3>Geração {index}</h3>
-      <ul>
-        {population.chromosomes.map((chromosome, index) => (
+      <ol>
+        {population.map(({ chromosome, fitness }, index) => (
           <li key={index}>
-            [{chromosome.map(gene => gene).join(', ')}] - {population.fitnesses[index]}
+            [{chromosome.map(gene => gene).join(', ')}] - R$ {fitness}
           </li>
         ))}
-      </ul>
+      </ol>
+      <p>Total Fitness: R$ {totalFitness}</p>
     </div>
   )
 }
@@ -28,24 +36,106 @@ type Props = {
 }
 
 const Home: NextPage<Props> = ({ knapsack }) => {
-  const { generations, items } = knapsack
-  return (
-    <main>
-      <h1>Problema da Mochila via Algoritmo Genético</h1>
-      <section>
-        <h2>Itens:</h2>
-        {items.map((item, index) => (
-          <Item key={index} label={`Item ${index + 1}`} {...item} />
-        ))}
-      </section>
-      <section>
-        <h2>Gerações</h2>
+  const [knapsackState, setKnapsackState] = useState(knapsack)
+  const [populationSize, setPopulationSize] = useState('10')
+  const [mutationTax, setMutationTax] = useState('30')
 
-        <div>
-          {generations.map(generation => (
-            <Generation key={generation.index} {...generation} />
-          ))}
-        </div>
+  const { generations, initialGeneration, items, mutations } = knapsackState
+
+  const handlePopulationSize = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (value.length > 3) return
+    const nValue = Number(value)
+    if (nValue < 8) return setPopulationSize('8')
+    if (nValue > 20) return setPopulationSize('20')
+    setPopulationSize(value)
+  }
+
+  const handleMutationTaxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (value.length > 3) return
+    if (Number(value) > 100) return setMutationTax('100')
+    setMutationTax(formatNumber(value))
+  }
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    setKnapsackState(solveKnapsack(Number(populationSize), Number(mutationTax) / 100))
+  }
+
+  return (
+    <main className={classes.wrap}>
+      <Head>
+        <title>Knapsack</title>
+      </Head>
+      <h1>Problema da Mochila via Algoritmo Genético</h1>
+      <section className={classes['initial-data']}>
+        <section>
+          <h2>Itens:</h2>
+          <ul>
+            {items.map((item, index) => (
+              <Item key={index} label={`Item ${index + 1}`} {...item} />
+            ))}
+          </ul>
+        </section>
+
+        <form onSubmit={handleSubmit} className={classes['inputs-form']}>
+          <h2>Parâmetros:</h2>
+          <div>
+            <label htmlFor="population-size-input">Tamanho da população (8~20):</label>
+            <input
+              id="population-size-input"
+              type="number"
+              value={populationSize}
+              onChange={handlePopulationSize}
+              placeholder="População"
+              min={8}
+              max={20}
+              maxLength={3}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="mutation-tax-input">Taxa de mutação:</label>
+            <div className={classes['mutation-tax-input__wrap']}>
+              <input
+                id="mutation-tax-input"
+                value={mutationTax}
+                onChange={handleMutationTaxChange}
+                placeholder="Taxa de mutação"
+                min={1}
+                max={100}
+                maxLength={3}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Executar</button>
+        </form>
+      </section>
+
+      <section className={classes.process}>
+        <section>
+          <h2>Gerações</h2>
+
+          <div className={classes.generations}>
+            <Generation {...initialGeneration} />
+            {generations.map(generation => (
+              <Generation key={generation.index} {...generation} />
+            ))}
+          </div>
+        </section>
+
+        <section className={classes.mutations}>
+          <div>
+            <h2>Mutações:</h2>
+            <ul className={classes['mutations-list']}>
+              {mutations.map((mutation, index) => (
+                <li key={index}>{`Geração ${mutation.generation} - Cromossomo ${mutation.chromosomeIndex}`}</li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </section>
     </main>
   )
